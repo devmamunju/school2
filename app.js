11d1969eea278349d93611c782429763fb2fd670
Md Al Mamun <mamunju2015@gmail.com>
2025-01-22 07:24:51 +0600

    add javascript

--- app.js
+++ app.js
@@ -15,3 +15,30 @@ window.addEventListener("scroll", function(){
 });
 
 });
+
+
+
+
+
+
+/**********************************/
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    
+    let arrow = document.querySelector(".arrow");
+    
+    window.addEventListener("scroll", function(){
+        if(window.scrollY > arrow.offsetHeight){
+    
+            arrow.classList.add("goup");
+    } else{
+    
+        arrow.classList.remove("goup");
+    
+    }
+    
+   
+    });
+    
+    });
